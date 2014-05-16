b34020f07aa74757163f9d6a1333729b7cf8911b
liling <maximuslee@126.com>
2014-05-16 15:52:57 +0800

    project init

---
+++ YouYang/YouYang/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YouYang
+//
+//  Created by 工业设计中意（湖南） on 14-5-16.
+//  Copyright (c) 2014年 中意工业设计（湖南）有限责任公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
